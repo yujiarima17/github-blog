ecf974241722419448830f1e70d00de5fc2a2d44
Yuji Arima <arimayuji2@gmail.com>
2023-11-17 23:23:51 -0300

    application theme done

---
+++ src/styles/global.ts
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+
+    :focus{
+        outline:0;
+    }
+    body{
+        background: ${props=>props.theme.colors['base-background']};
+        -webkit-font-smoothing : antialiased;
+    }
+}`
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,26 @@
+export const defaultTheme ={
+    colors:{
+        blue:'#3294F8' ,
+        'base-title':'#E7EDF4',
+        'base-subtitle':'#C4D4E3',
+        'base-text':'#AFC2D4',
+        'base-span':'#7B96B2',
+        'base-label':'#3A536B',
+        'base-border':'#1C2F41',
+        'base-post':'#112131',
+        'base-profile':'#0B1B2B',
+        'base-background':'#071422',
+        'base-input':'#040F1A',
+
+    },
+    fonts:{
+        'Title L': 'bold 24px/130% Nunito,sans-serif',
+        'Title M': 'bold 20px/160% Nunito,sans-serif',
+        'Title S': 'bold 18px/160% Nunito,sans-serif',
+        'Text M':'16px/160% Nunito,sans-serif',
+        'Text S':'14px/160% Nunito,sans-serif',
+        'Link': 'bold 12px/1605 Nunito,sans-serif',
+
+    }
+
+}
